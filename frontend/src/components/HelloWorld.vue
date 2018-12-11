bb3332f7dafa1b7d82bedb33bb282864535444cb
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-12-11 14:51:49 +0530

    vue frontend service for UI

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="myApplication">
+    <h1>Hello World</h1>
+    <p>little text behind the title</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+  #HelloWorld {
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
